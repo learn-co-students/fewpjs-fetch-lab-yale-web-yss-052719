7f7b9b560f4fcfa0e2a5cd9531533dd085adf2c4
jqzheng617 <jenna.zheng617@gmail.com>
2019-07-11 22:27:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 function fetchBooks() {
-
-}
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => {
+    renderBooks(json)
+  })
+  }
 
 function renderBooks(json) {
   const main = document.querySelector('main')
